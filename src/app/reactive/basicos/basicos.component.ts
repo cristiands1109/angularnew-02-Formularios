98bafef18d7449b7b4fde813c5a185ed2de1f429
Christian Duarte <cristiands1109@gmail.com>
2021-04-16 20:10:26 -0400

    Fin de la Seccion 17 - Formulario Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre:     new FormControl('Lapiz'),
+  //   precio:     new FormControl(2000),
+  //   existencias: new FormControl(20),
+    
+  // })
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]]
+  })
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+
+      nombre: 'Lapiz',
+      precio: 2000,
+      
+    })
+  }
+
+  noEsValido(campo: string) {
+    return this.miFormulario.controls[campo].invalid && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
